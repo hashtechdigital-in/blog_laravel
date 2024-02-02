88ae6c11daa4e8f6f31952b2e17611018e8100bc
vishal arora <vishalarora1087@gmill.com>
2024-02-03 00:21:29 +0530

    front page links

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -3,7 +3,7 @@
       <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container box_1620">
           <!-- Brand and toggle get grouped for better mobile display -->
-          <a class="navbar-brand logo_h" href="index.html"><img src="{{ url('front/img/logo.png') }}" alt=""></a>
+          <a class="navbar-brand logo_h" href="{{ url('index') }}"><img src="{{ url('front/img/logo.png') }}" alt=""></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -12,17 +12,17 @@
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
             <ul class="nav navbar-nav menu_nav justify-content-center">
-              <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li> 
-              <li class="nav-item"><a class="nav-link" href="archive.html">Archive</a></li> 
-              <li class="nav-item"><a class="nav-link" href="category.html">Category</a>
+              <li class="nav-item active"><a class="nav-link" href="{{ url('index') }}">Home</a></li> 
+              <li class="nav-item"><a class="nav-link" href="{{ url('archive') }}">Archive</a></li> 
+              <li class="nav-item"><a class="nav-link" href="{{ url('category') }}">Category</a>
               <li class="nav-item submenu dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                   aria-expanded="false">Pages</a>
                 <ul class="dropdown-menu">
-                  <li class="nav-item"><a class="nav-link" href="blog-details.html">Blog Details</a></li>
+                  <li class="nav-item"><a class="nav-link" href="{{ url('blog-details') }}">Blog Details</a></li>
                 </ul>
               </li>
-              <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+              <li class="nav-item"><a class="nav-link" href="{{ url('contact') }}">Contact</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right navbar-social">
               <li><a href="#"><i class="ti-facebook"></i></a></li>
