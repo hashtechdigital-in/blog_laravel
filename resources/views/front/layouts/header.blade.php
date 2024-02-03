beb1dffc67794989932bdff07bdad8e1a2368c59
vishal arora <vishalarora1087@gmill.com>
2024-02-03 14:31:16 +0530

    package breez, auth

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -23,6 +23,7 @@
                 </ul>
               </li>
               <li class="nav-item"><a class="nav-link" href="{{ url('contact') }}">Contact</a></li>
+              <li class="nav-item"><a class="nav-link" href="{{ url('admin') }}">Admin</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right navbar-social">
               <li><a href="#"><i class="ti-facebook"></i></a></li>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::match(['get', 'post'],'/','IndexController@indexHome');
 Route::get('archive','IndexController@archiveHome');
 Route::get('category','IndexController@categoryHome');
 Route::get('blog-details','IndexController@blog_detailsHome');
 Route::get('contact','IndexController@contactHome');
+
+require __DIR__.'/auth.php';
